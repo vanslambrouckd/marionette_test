b3d92da37a4238e97c1470d0f2742ccbdef6caf0
Paul Miller <=>
2014-12-18 12:05:13 +0100

    composite view

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ var ContactManager = new Marionette.Application();
 ContactManager.addRegions({
     mainRegion: '#main-region'
 });
+
+ContactManager.on('start', function() {
+    ContactManager.ContactsApp.List.Controller.listContacts();
+});
--- js/list_view.js
+++ js/list_view.js
@@ -1,11 +1,26 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _) {
     List.Contact = Marionette.ItemView.extend({
-        tagName: 'li',
+        tagName: 'tr',
         template: '#contact-list-item'
     });
 
+    /*
     List.Contacts = Marionette.CollectionView.extend({
         tagName: 'ul',
         childView: List.Contact
+    });
+	*/
+
+    /*
+    composite view:
+    om de tbody itemcollection te wrappen in table en theader tag
+    (=samengestelde view)
+    */
+    List.Contacts = Marionette.CompositeView.extend({
+        tagName: 'table',
+        className: 'table table-hover',
+        template: '#contact-list',
+        childView: List.Contact,
+        itemViewContainer: 'tbody'
     });
 });
