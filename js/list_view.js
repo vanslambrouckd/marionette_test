335429ccb7e2d9245d7ba14b56a6fcfe11226205
Paul Miller <=>
2014-12-18 12:19:53 +0100

    events

--- js/list_view.js
+++ js/list_view.js
@@ -1,7 +1,19 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _) {
     List.Contact = Marionette.ItemView.extend({
         tagName: 'tr',
-        template: '#contact-list-item'
+        template: '#contact-list-item',
+        events: {
+            'click': 'highlightName',
+            'click .jsDelete': 'deleteContact'
+        },
+        highlightName: function(event) {
+            event.preventDefault();
+            this.$el.toggleClass('warning');
+        },
+        deleteContact: function(event) {
+            event.stopPropagation(); //zodat highlightname niet uitgevoerd wordt, (event bubbling)
+            this.model.collection.remove(this.model);
+        }
     });
 
     /*
@@ -18,7 +30,7 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
     */
     List.Contacts = Marionette.CompositeView.extend({
         tagName: 'table',
-        className: 'table table-hover',
+        className: 'ui table',
         template: '#contact-list',
         childView: List.Contact,
         childViewContainer: 'tbody'
