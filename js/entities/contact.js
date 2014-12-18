a27476e883321cdd39d092473b06f50e44d8c30a
Paul Miller <=>
2014-12-18 14:42:19 +0100

    artificial loading view and deferreds

--- js/entities/contact.js
+++ js/entities/contact.js
@@ -40,20 +40,48 @@ ContactManager.module('Entities', function(Entities, ContactManager, Backbone, M
 
     Entities.getContactEntities = function() {
         var contacts = new Entities.ContactCollection();
-        contacts.fetch();
-        if (contacts.length === 0) {
-            console.log('initializeContacts');
-            return initializeContacts();
-        }
-        return contacts;
+
+        var defer = $.Deferred();
+        setTimeout(function() {
+            contacts.fetch({
+                success: function(data) {
+                    defer.resolve(data);
+                }
+            });
+        }, 300);
+
+
+        var promise = defer.promise();
+        $.when(promise).done(function(contacts) {
+            console.log(contacts.length);
+            if (contacts.length === 0) {
+                console.log('initializeContacts');
+                var models = initializeContacts();
+                contacts.reset(models);
+
+            }
+        });
+        return promise;
     }
 
     Entities.getContactEntity = function(contactId) {
         var contact = new Entities.Contact({
             id: contactId
         });
-        contact.fetch();
-        return contact;
+
+        var defer = $.Deferred();
+        //test: dealing with latency
+        setTimeout(function() {
+            contact.fetch({
+                success: function(data) {
+                    defer.resolve(data);
+                },
+                error: function(data) {
+                    defer.resolve(undefined);
+                }
+            })
+        }, 2000);
+        return defer.promise();
     }
 
     ContactManager.reqres.setHandler('contact:entity', function(id) {
--- js/list_controller.js
+++ js/list_controller.js
@@ -1,27 +1,33 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _) {
     List.Controller = {
         listContacts: function() {
-            var contacts = ContactManager.request('contact:entities');
 
-            var contactsListView = new List.Contacts({
-                collection: contacts
+            var loadingView = new ContactManager.Common.Views.Loading();
+            ContactManager.mainRegion.show(loadingView);
+
+            var fetchingContacts = ContactManager.request('contact:entities');
+
+            $.when(fetchingContacts).done(function(contacts) {
+                var contactsListView = new List.Contacts({
+                    collection: contacts
+                });
+
+                contactsListView.on('childview:contact:delete', function(childView, model) {
+                    model.destroy();
+                });
+
+                contactsListView.on('childview:contact:highlight', function(childView, model) {
+                    console.log('highlighting toggled on model: ', model);
+                });
+
+                contactsListView.on('childview:contact:show', function(childView, model) {
+                    console.log('received childview:contact:show event on model', model);
+
+                    ContactManager.trigger('contact:show', model.get('id'));
+                });
+
+                ContactManager.mainRegion.show(contactsListView);
             });
-
-            contactsListView.on('childview:contact:delete', function(childView, model) {
-                model.destroy();
-            });
-
-            contactsListView.on('childview:contact:highlight', function(childView, model) {
-                console.log('highlighting toggled on model: ', model);
-            });
-
-            contactsListView.on('childview:contact:show', function(childView, model) {
-                console.log('received childview:contact:show event on model', model);
-
-                ContactManager.trigger('contact:show', model.get('id'));
-            });
-
-            ContactManager.mainRegion.show(contactsListView);
         }
     }
 });
