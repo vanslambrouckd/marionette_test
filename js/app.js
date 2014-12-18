6790d2f927a418a93fb591cc026ec874a53c911b
Paul Miller <=>
2014-12-18 13:47:32 +0100

    route refactoring

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ ContactManager.addRegions({
     mainRegion: '#main-region'
 });
 
+ContactManager.navigate = function(route, options) {
+    options = options || {};
+    Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function() {
+    return Backbone.history.fragment;
+};
+
 ContactManager.on('start', function() {
     /*
     ContactManager.ContactsApp.List.Controller.listContacts();
@@ -10,8 +19,8 @@ ContactManager.on('start', function() {
     if (Backbone.history) {
         Backbone.history.start();
 
-        if (Backbone.history.fragment === '') { //if no #contacts
-            Backbone.history.navigate('contacts'); //update url fragment
+        if (this.getCurrentRoute() === '') { //if no #contacts
+            ContactManager.navigate('contacts'); //update url fragment
             ContactManager.ContactsApp.List.Controller.listContacts(); //show contacts
         }
     }
