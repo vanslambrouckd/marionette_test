98d0cd0189d021a60ffaa333452e0e98cbb9c4ae
Paul Miller <=>
2014-12-19 11:00:42 +0100

    regions

--- js/list_controller.js
+++ js/list_controller.js
@@ -10,10 +10,18 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
 
             var fetchingContacts = ContactManager.request('contact:entities');
 
+            var contactsListLayout = new List.Layout();
+            var contactsListPanel = new List.Panel();
+
             $.when(fetchingContacts).done(function(contacts) {
                 var contactsListView = new List.Contacts({
                     collection: contacts
                 });
+
+                contactsListLayout.on('show', function() {
+                    contactsListLayout.panelRegion.show(contactsListPanel);
+                    contactsListLayout.contactsRegion.show(contactsListView);
+                })
 
                 contactsListView.on('childview:contact:delete', function(childView, model) {
                     model.destroy();
@@ -48,7 +56,7 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
                     ContactManager.modalRegion.show(view);
                 });
 
-                ContactManager.mainRegion.show(contactsListView);
+                ContactManager.mainRegion.show(contactsListLayout);
             });
         }
     }
